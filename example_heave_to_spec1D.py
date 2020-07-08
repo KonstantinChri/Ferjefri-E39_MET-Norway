22aa1fc77882aeb7df46aa07098b1350de60c3f6
kostasxas <67804784+kostasxas@users.noreply.github.com>
2020-07-08 14:21:43 +0200

    Update example_heave_to_spec1D.py

--- example_heave_to_spec1D.py
+++ example_heave_to_spec1D.py
@@ -9,10 +9,9 @@ from Spec_functions import heave_to_spec1D
 # Provide raw wave data from thredds.met.no/thredds/
 ds_raw = xr.open_dataset('https://thredds.met.no/thredds/dodsC/obs/buoy-svv-e39/2020/01/202001_E39_Svinoy_raw_wave.nc')
 #ds_raw = xr.open_dataset('https://thredds.met.no/thredds/dodsC/obs/buoy-svv-e39/2017/11/201711_E39_F_Vartdalsfjorden_raw_wave.nc')
-#ds_raw = xr.open_dataset('Data/201711_201804_E39_F_Vartdalsfjorden_raw_wave.nc')
 
 
-# Provide desired frequencies
+# Provide the frequency grid
 freq_new = np.arange(0.04,0.50,0.01)
 # Call heave_to_spec1D function
 ds = heave_to_spec1D(ds_raw.heave,freq_new,detrend = True)
@@ -22,17 +21,16 @@ Hs_heave =  4*np.std(ds_raw.heave,axis=1)
 Hs_raw =  4*(ds.SPEC_raw.integrate("freq_raw"))**0.5
 Hs_inter =  4*(ds.SPEC_new.integrate("freq_new"))**0.5
 
-# time step for maximum Hs
-time_step = np.where(Hs_heave==Hs_heave.max())[0].tolist()
-#max_Hs = Hs_heave.where(Hs_heave==Hs_heave.max(),drop=True).squeeze()
+# maximum Hs
+max_Hs = Hs_heave.where(Hs_heave==Hs_heave.max(),drop=True).squeeze()
 
 # Plot Spec1D for given time step
 plt.figure()
-plt.plot(ds.freq_raw, ds.SPEC_raw[time_step[0],:],color='grey',label='Raw-Spec, Hm0[m]:'+str(Hs_raw.values[time_step[0]].round(2))+',4*std(heave)[m]:'+str(Hs_heave.values[time_step[0]].round(2)))
-plt.plot(ds.freq_new, ds.SPEC_new[time_step[0],:],color='red',label='Interpol.-Raw-Spec., Hm0[m]:'+str(Hs_inter.values[time_step[0]].round(2)))
+plt.plot(ds.freq_raw, ds.SPEC_raw.loc[max_Hs.time],color='grey',label='Raw-Spec, Hm0[m]:'+str(Hs_raw.loc[max_Hs.time].values.round(2))+',4*std(heave)[m]:'+str(Hs_heave.loc[max_Hs.time].values.round(2)))
+plt.plot(ds.freq_new, ds.SPEC_new.loc[max_Hs.time],color='red',label='Interpol.-Raw-Spec., Hm0[m]:'+str(Hs_inter.loc[max_Hs.time].values.round(2)))
 plt.grid()
 plt.legend()
-plt.title('time:'+str(time_step[0]))
+plt.title('time:'+str(max_Hs.time.values)[:19])
 plt.xlabel('freq.[Hz]')
 plt.ylabel('SPEC.[m**2 s]')
 plt.show()
@@ -43,7 +41,7 @@ Hs_heave.plot(marker='.',label='4*std(heave)')
 Hs_inter.plot(marker='.',label='4*sqrt(m0_interp)') 
 plt.xlabel('time')
 plt.ylabel('Hm0[m]')
-plt.title('Max-Hs at time:'+str(time_step[0]))
+plt.title('Max-Hs at time:'+str(max_Hs.time.values)[:19])
 plt.grid()
 plt.legend()
 plt.show()

6e90fda74d201d422586484fcae4d7b3e5aa186f
KonstantinChri <kchrist87@gmail.com>
2021-12-25 23:36:31 +0100

    update example

--- download_E39_ncfiles.py
+++ download_E39_ncfiles.py
@@ -1,22 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from urllib.request import urlretrieve
-import pandas as pd
-
-buoy = 'B'
-fjord = 'Sulafjorden' # Sulafjorden / Breisundet etc
-variable = 'aquadopp' # wave / wind / aquadopp
-start_date = '2016-10'
-end_date = '2020-07'
-dir_for_download = '...'+buoy+'/'+variable+'/'
+from Spec_functions import download_E39_obs
 
 
-dates = pd.period_range(start_date,end_date, freq="M")
-for i in range(len(dates)):
-    filein = dates[i].strftime('%Y') + '/' + dates[i].strftime('%m') + '/'+ dates[i].strftime('%Y') + dates[i].strftime('%m') + '_E39_'+buoy +'_'+fjord+'_'+variable +'.nc'
-    url_thredds = 'https://thredds.met.no/thredds/fileServer/obs/buoy-svv-e39/'+filein
-    print(url_thredds)
-    urlretrieve(url_thredds,dir_for_download+filein.split('/')[2])
-
-#Merge nc files
-#ncrcat *.nc -O  2016_2020_E39_B_Sulafjorden_aquadopp.nc
+download_E39_obs(start_date='2019-12',end_date='2020-02',buoy='B',
+fjord='Sulafjorden',variable='wave')

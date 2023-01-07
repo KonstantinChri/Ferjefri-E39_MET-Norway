d1a57978951337279cea7dbbf82446d156f6f5d3
KonstantinChri <konstantinosc@met.no>
2023-01-07 21:54:10 +0100

    update example

--- example_raw_wave_to_spec2D.py
+++ example_raw_wave_to_spec2D.py
@@ -8,7 +8,7 @@ from Spec_functions import Directional_Spectra
 time0='2020-01-09T00:00'
 ds_raw = xr.open_dataset('https://thredds.met.no/thredds/dodsC/obs/buoy-svv-e39/2020/01/202001_E39_Svinoy_raw_wave.nc')
 
-ds = Directional_Spectra(raw_data = ds_raw, freq_resolution= 0.01,n_direction= 72,sample_frequency= 1)
+ds = Directional_Spectra(raw_data = ds_raw, freq_resolution= 0.01,n_direction= 72,sample_frequency= 1,direction_units='rad')
 
 Hs_heave =  4*np.std(ds_raw.heave,axis=1)
 Hs_spec = 4*(np.sum(ds.SPEC.integrate("frequency"),axis=1))**0.5
@@ -23,6 +23,3 @@ plt.legend()
 
 plt.figure(1)
 ds.SPEC.loc[time0].plot.pcolormesh()
-
-
-
